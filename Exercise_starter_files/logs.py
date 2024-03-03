b8e0843f0957bf126c1b52c0131c1753de0ee41c
Tony Patton <aspatton@users.noreply.github.com>
2024-03-03 01:39:21 -0500

    Update logs.py

--- Exercise_starter_files/logs.py
+++ Exercise_starter_files/logs.py
@@ -5,7 +5,7 @@ from azureml.core.webservice import Webservice
 ws = Workspace.from_config()
 
 # Set with the deployment name
-name = ""
+name = "UdacityProject2Deployment"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
